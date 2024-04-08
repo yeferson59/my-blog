faf92bb9d0f96a7bdf713d0c603baa089935c129
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-04-08 09:45:25 -0500

    added taillwind and astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import db from "@astrojs/db";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), db()]
+});
